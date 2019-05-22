f8f31fe1e124963ab3a37c9112c5de2cfaa75b53
Jared Avila <jared.c.avila@gmail.com>
2019-05-22 12:51:32 -0700

    higher order componnents

--- src/components/People/Person/Person.js
+++ src/components/People/Person/Person.js
@@ -1,26 +1,30 @@
 import React, { Component } from "react";
 import "./Person.css";
 import classes from "./Person.css";
+import Auxiliary from "../../../hoc/Auxiliary";
 
 class Person extends Component {
   render() {
     console.log("[Person] rendering");
     return (
-      <div className={classes.Person}>
-        <p
-          onClick={() => this.props.click("James Bond")}
-          className={classes.people}
-        >
-          I'm {this.props.name} and I'm {this.props.age}
-        </p>
-        <p>{this.props.children}</p>
-        <input
-          type="text"
-          onChange={this.props.changed}
-          placeholder={this.props.name}
-          className={classes.bold}
-        />
-      </div>
+      <Auxiliary>
+        <div className={classes.Person}>
+          <p
+            onClick={() => this.props.click("James Bond")}
+            className={classes.people}
+          >
+            I'm {this.props.name} and I'm {this.props.age}
+          </p>
+          <p>{this.props.children}</p>
+          <input
+            type="text"
+            onChange={this.props.changed}
+            placeholder={this.props.name}
+            className={classes.bold}
+          />
+        </div>
+        <h1>Adjacent element</h1>
+      </Auxiliary>
     );
   }
 }
